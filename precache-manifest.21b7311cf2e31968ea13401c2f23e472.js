b9ab6287c3063a4d39aa24d36513ff4757ace017
bmgreenfield83 <45639721+bmgreenfield83@users.noreply.github.com>
2020-01-26 08:20:03 -0500

    Updates

---
+++ precache-manifest.21b7311cf2e31968ea13401c2f23e472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c320ddc74c2099dc474e57372fd14f8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9b02d9da6c519dfcb768",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "08b31f1dbbbacaf5ebbc",
+    "url": "/robofriends/static/css/main.e9b9fe4b.chunk.css"
+  },
+  {
+    "revision": "9b02d9da6c519dfcb768",
+    "url": "/robofriends/static/js/2.cc06539d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.cc06539d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08b31f1dbbbacaf5ebbc",
+    "url": "/robofriends/static/js/main.e092f8f0.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
